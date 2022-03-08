59f8e41762ce58a25ee66e834ede1d3431568d1a
Gricel <63869574+Gricel-lee@users.noreply.github.com>
2022-03-08 18:25:34 +0100

    example

---
+++ runtime-EclipseXtext/demoQuartet/src-gen/Example1.mydsl.pm
@@ -0,0 +1,9 @@
+m1: WARNING. Translation into Prism not supported.
+
+m2: R{"Time"}min=?[F "r1goal"]
+
+m3: (A[G"close" => ((F "r1CP") & (F "r1TA") & (F "r1HA"))]) &
+((A[G"record" <=> (X"CP")]) & ((A[G"record" <=> (X"TA")]) &
+(A[G"record" <=> (X"HA")] ) ) )
+
+
